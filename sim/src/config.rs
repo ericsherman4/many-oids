e487d36a7cfbf2500181961311b1d301651d6399
Eric Sherman <44278520+ericsherman4@users.noreply.github.com>
2024-12-15 19:37:19 -0500

    Convert everything to use Bevy's plugin system (#1)
    * convert everything to plugins
    
    * formatting

--- sim/src/config.rs
+++ sim/src/config.rs
@@ -1,10 +1,7 @@
-
 /// Scene settings
 pub mod colors_config {
     use bevy::prelude::Color;
     use bevy::prelude::Srgba;
-
-    pub const BG_COLOR: &str = "000000";
 
     pub const BLUE: Color = Color::Srgba(Srgba::BLUE);
     pub const GREEN: Color = Color::Srgba(Srgba::GREEN);
@@ -19,21 +16,33 @@ pub mod colors_config {
 pub mod lights_config {
     use bevy::math::Vec3;
 
-    pub const POS_COMPONENT: f32 = 10.0;
-    pub const POS: Vec3 = Vec3::splat(POS_COMPONENT);
-    pub const POS_2: Vec3 = Vec3::new(-7.2, -10.4, -8.8);
+    pub const BG_COLOR: &str = "000000";
+    pub const GIZMOS_ON: bool = false;
+
+    // Light 1
+    pub const POS_1: Vec3 = Vec3::splat(7.0);
+    pub const LOOKING_AT_1: Vec3 = Vec3::ZERO;
+
+    //Light 2
+    pub const POS_2: Vec3 = Vec3::splat(-7.0);
+    pub const LOOKING_AT_2: Vec3 = Vec3::ZERO;
 }
 
+pub mod origin_config {
+    use bevy::prelude::Color;
+    use bevy::prelude::Srgba;
 
-// TODO: you should make the draw axes stuff into a a plugin
-pub mod axis_config {
-    pub const GIRTH: f32 = 0.05;
-    pub const LENGTH: f32 = 10.;
-    pub const HALF_LENGTH: f32 = LENGTH / 2.;
-    pub const ORIGIN_SPHERE_RADIUS: f32 = GIRTH;
+    pub const AXIS_GIRTH: f32 = 0.05;
+    pub const AXIS_LENGTH: f32 = 10.;
+    pub const ORIGIN_SPHERE_RADIUS: f32 = AXIS_GIRTH;
+
+    pub const COLOR_Z: Color = Color::Srgba(Srgba::BLUE);
+    pub const COLOR_Y: Color = Color::Srgba(Srgba::GREEN);
+    pub const COLOR_X: Color = Color::Srgba(Srgba::RED);
 }
 
 pub mod cam_config {
     use bevy::math::Vec3;
     pub const POS: Vec3 = Vec3::splat(7.0);
+    pub const LOOKING_AT: Vec3 = Vec3::ZERO;
 }
--- sim/src/main.rs
+++ sim/src/main.rs
@@ -1,28 +1,19 @@
 use bevy::prelude::*;
-
-
-use smooth_bevy_cameras::controllers::unreal::UnrealCameraPlugin;
-use smooth_bevy_cameras::LookTransformPlugin;
+use smooth_bevy_cameras::{controllers::unreal::UnrealCameraPlugin, LookTransformPlugin};
 
 mod config;
-use config::colors_config;
 mod scene;
 
 fn main() {
     App::new()
-        // bevy's entire engine is written as plugins so maybe your stuff should be too? 
+        // bevy's entire engine is written as plugins so maybe your stuff should be too?
         .add_plugins((
             DefaultPlugins,
             LookTransformPlugin,
             UnrealCameraPlugin::default(),
+            scene::CustomLightsPlugin,
+            scene::OriginPlugin,
+            scene::CustomCameraPlugin,
         ))
-
-        .insert_resource(ClearColor(colors_config::get_color(config::colors_config::BG_COLOR)))
-
-        .add_systems(Startup, scene::setup)
-        .add_systems(Startup, scene::draw_xyz)
-
         .run();
-
-
 }
